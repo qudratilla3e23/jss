9327c98a58e84a3424ceeffe631a56f6d494ca21
qudratilla3e23 <abdullayevqudrat224@gmail.com>
2025-08-01 10:25:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// console.log("ffsfsg,sgsggsgsgs.sgsgsgsr");
+// console.log(324569);
+
+// confirm("O'rnatilsimi")
+// prompt("ism kiriting")
+
+let ism=prompt("ism kiriting")
+let yosh=prompt("yoshingizni kiriting")
+let familya=prompt("familya kiriting")
+let fan=prompt("yogtirgan faningiz")
+
+
+console.log("ism",ism);
+console.log("yosh",yosh);
+console.log("familya",familya);
+console.log("fan",fan);
+
+
+alert("Malumot uchun tashakur")
